d7b06a7e26b64e080a8a7aec1d78e58661d4a1a6
Domalega Vladimir <60595331+Domalega@users.noreply.github.com>
2021-02-19 19:23:25 +0700

    Add file.py

---
+++ PythonApplication1.py
@@ -0,0 +1,15 @@
+import requests
+import re
+
+def main() -> list:
+    requestvk = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172')
+    ctfflags = re.findall( '\w\w\w[{]\w+[}]', requestvk.text)
+
+    file = open('CTF.txt', "w")
+    for element in ctfflags:
+        file.write(element)
+        file.write('\n')
+    file.close()
+    return ctfflags
+
+main()
