9072e1adc8eb03d94c8a74370f1d3e8a07b8c138
Domalega Vladimir <60595331+Domalega@users.noreply.github.com>
2021-03-22 01:53:12 +0700

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,6 +1,7 @@
 import requests
 import re
 
+
 def main() -> list:
     requestvk = requests.get('https://vk.com/lifetrainings?w=wall-171787766_172')
     ctfflags = re.findall( '\w\w\w[{]\w+[}]', requestvk.text)
