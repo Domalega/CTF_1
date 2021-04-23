e88e7fb713950db99102fddbc4703c917b06b08a
Domalega Vladimir <60595331+Domalega@users.noreply.github.com>
2021-04-23 15:25:51 +0700

    Update лесенка.py

--- Учим питон v0.1/лесенка.py
+++ Учим питон v0.1/лесенка.py
@@ -1,12 +1,20 @@
 def ladder():
-    a = int(input())
-    choose = input("влево или вправо: \n")
-    if choose == "влево":
-        for i in range(0, a + 1):
-            print(" " * (a - i) + "*" * i)
+  a = int(input())
+  choose = input("1, 2, 3, 4: \n")
+  if choose == "1":
+    for i in range(0, a + 1):
+      print(" " * (a - i) + "*" * i)
 
-    elif choose == "вправо":    
-        for i in range(0, a + 1):
-            print("*" * i + " " * (a - i)) 
+  elif choose == "2":    
+    for i in range(0, a + 1):
+        print("*" * i + " " * (a - i))
+        
+  elif choose == "3":    
+    for i in range(0, a + 1):
+      print("*" * (a - i) + " " * (a - i)) 
+            
+  elif choose == "4":    
+    for i in range(0, a + 1):
+      print(" " * a + "*" * (a - i)) 
 
 ladder()
